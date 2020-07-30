82ec1eae1cde1fd6210d491e72f11cd24c266d78
Jens Peters <jp7677@gmail.com>
2020-07-30 09:26:38 +0200

    Pull latest before rebuilding the image.

--- setup-dxvk-docker.sh
+++ setup-dxvk-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker pull archlinux/base:latest
 docker rm $(docker ps -a -q --filter ancestor=dxvk-docker) 2> /dev/null
 docker rmi dxvk-docker:latest 2> /dev/null
 docker build . -t dxvk-docker:latest

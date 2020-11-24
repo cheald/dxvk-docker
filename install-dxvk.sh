acb7590c369207bc6cec88a87972574828a54549
Jens Peters <jp7677@gmail.com>
2020-11-24 21:34:47 +0100

    Remove old versions first.

--- install-dxvk.sh
+++ install-dxvk.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/dxgi.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/d3d11.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/d3d10core.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/d3d10.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/d3d10_1.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/d3d9.dll
+
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/dxgi.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d11.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d10core.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d10.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d10_1.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d9.dll
+
 cp --verbose out/dxvk-master/x64/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
 cp --verbose out/dxvk-master/x64/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
 cp --verbose out/dxvk-master/x64/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
--- install-vkd3d.sh
+++ install-vkd3d.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+rm ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/d2d12.dll
+rm ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/d2d12.dll
+
 cp --verbose out/vkd3d-proton-master/x64/d2d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/
-
 cp --verbose out/vkd3d-proton-master/x86/d2d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/
-

76456e9e9c4d7bd51e3a8620977bc3fbe6d515b2
Jens Peters <jp7677@gmail.com>
2020-12-04 09:34:00 +0100

    Use podman when available.

--- make-dxvk.sh
+++ make-dxvk.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+if [ -x "$(command -v podman)" ]; then
+    shopt -s expand_aliases
+    alias docker=podman
+fi
+
 rm -Rf out/* && docker run --rm -e UID=$UID -e GID=$(id -g $USER) -it -v $(pwd)/dxvk:/root/build -v $(pwd)/out:/root/build/out dxvk-docker:latest
--- setup-dxvk-docker.sh
+++ setup-dxvk-docker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -x "$(command -v podman)" ]; then
+    shopt -s expand_aliases
+    alias docker=podman
+fi
+
 docker pull archlinux/base:latest
 docker rmi dxvk-docker:latest 2> /dev/null
 docker build . -t dxvk-docker:latest
